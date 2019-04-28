c59873e1c1814cf340d8b060c0724ac007d33358
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-04-29 03:38:32 +0500

    Update test.js
    Adding a test for the createNewTransaction method

--- dev/test.js
+++ dev/test.js
@@ -6,4 +6,6 @@ bitcoin.createNewBlock(32342, "HSKJD78864TG#$778", "SHGF67rt34TUYFDG");
 bitcoin.createNewBlock(234342, "HSKJD78864TG#$778", "SHGF67we34TUYFDG");
 bitcoin.createNewBlock(43342, "HSKJD78864TG#$778", "SHGF673344TUYFDG");
 
+bitcoin.createNewTransaction(100, "AMIRGH76SJFG7634", "ZAFAR243HGF24343U");
+
 console.log(bitcoin);
