b6fa7b647cc22f21c54b123d9b0eb65121df3112
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-06-17 08:08:32 +0500

    Update api.js
    Wiring up blockchain constructor with the blockchain route on the server

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,19 @@
+// Importing the express library
 const express = require("express");
 const app = express();
+
+// Importing our blockchain contrunctor
+const Blockchain = require("./blockchain");
+
+// Initializing our blockchain on the server
+let bitcoin = new Blockchain();
+
+// Specifying the port number on which server runs
 const port = 3000;
+
+// Adding body parser as a middlewear to
+// all the requests send to server
+// Its makes extracting the data from the request easy
 var bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
@@ -13,7 +26,10 @@ app.use(
 // parse application/json
 app.use(bodyParser.json());
 
-app.get("/blockchain", (req, res) => res.send("Hello World!"));
+// Sending back our complete blockchain to the client
+app.get("/blockchain", (req, res) => res.send(bitcoin));
+
+
 app.post("/transaction", (req, res) => {
   console.log(req.body);
   res.send(`Amount is ${req.body.amount} and sender is ${req.body.sender}`);
