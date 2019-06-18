6ad8a9235b6f0e59a8b43fc556ad504c5db08969
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-06-18 22:25:06 +0500

    Update blockchain.js
    Adding a new method to validate the blockchain chainIsValid and test that

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -75,12 +75,12 @@ Blockchain.prototype.addTransactionToPendingTransactions = function(
 //Hashing a block method using sha256
 Blockchain.prototype.hashBlock = function(
   previousBlockHash,
-  currrentBlockData,
+  currentBlockData,
   nonce
 ) {
   // Creating string for the sha256 Algorithm to generate hash
   const dataAsString =
-    previousBlockHash + nonce.toString() + JSON.stringify(currrentBlockData);
+    previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
   const hash = sha256(dataAsString);
   return hash;
 };
@@ -88,16 +88,68 @@ Blockchain.prototype.hashBlock = function(
 //Adding Proof of work method
 Blockchain.prototype.proofOfWork = function(
   previousBlockHash,
-  currrentBlockData
+  currentBlockData
 ) {
   let nonce = 0;
-  let hash = this.hashBlock(previousBlockHash, currrentBlockData, nonce);
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
   while (hash.substring(0, 2) !== "00") {
     nonce++;
-    hash = this.hashBlock(previousBlockHash, currrentBlockData, nonce);
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
     // console.log(hash);
   }
   return nonce;
+};
+
+// Doing consenses algorithm
+// Validating if a blockchain contains correct data
+Blockchain.prototype.chainIsValid = function(blockchain) {
+  // Initially chain is valid
+  let validChain = true;
+
+  // Performing for loop through blockchain
+  // Checking each block data
+  // We are skiping genesis block here
+  for (let i = 1; i < blockchain.length; i++) {
+    const currentBlock = blockchain[i],
+      prevBlock = blockchain[i - 1],
+      // Generating the block hash again to see that if the data inside a block
+      // is correct
+      blockHash = this.hashBlock(
+        prevBlock["hash"],
+        {
+          transactions: currentBlock["transactions"],
+          index: currentBlock["index"]
+        },
+        currentBlock["nonce"]
+      );
+
+    // Checking current block hash with previous block hash
+    // to see if that block is correct
+    if (
+      currentBlock["previousBlockHash"] !== prevBlock["hash"] ||
+      blockHash.substring(0, 2) !== "00"
+    ) {
+      validChain = false;
+    }
+  }
+
+  // Validating the genesis block
+  const genesisBlock = blockchain[0];
+  const correctNonce = genesisBlock["nonce"] === 100;
+  const correctPreviousBLockHash = genesisBlock["previousBlockHash"] === "0";
+  const correctHash = genesisBlock["hash"] === "0";
+  const correctTransactions = genesisBlock["transactions"].length === 0;
+
+  if (
+    !correctNonce ||
+    !correctPreviousBLockHash ||
+    !correctHash ||
+    !correctTransactions
+  ) {
+    validChain = false;
+  }
+  // returning state of chain valid or not valid
+  return validChain;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -82,3 +82,40 @@ console.log(bitcoin.getLastBlock());
 //     }
 //   ])
 // );
+
+const chain = [
+  {
+    index: 1,
+    timestamp: 1560873007703,
+    transactions: [],
+    nonce: 100,
+    hash: "0",
+    previousBlockHash: "0"
+  },
+  {
+    index: 2,
+    timestamp: 1560873023658,
+    transactions: [],
+    nonce: 363,
+    hash: "005c850455f8bf8801a8901d9908cb6acd33bc49c9d718fcf30867c3cbc91ee3",
+    previousBlockHash: "0"
+  },
+  {
+    index: 3,
+    timestamp: 1560873036985,
+    transactions: [
+      {
+        amount: 12.5,
+        sender: "00",
+        recipient: "bf244d8091e011e99bac77964cb20fc9",
+        transactionId: "c8a85e5091e011e99bac77964cb20fc9"
+      }
+    ],
+    nonce: 68,
+    hash: "009f9066efab60f2798a004eb79e21c4ba2df35dfc900581f3888cbcb48a0fb2",
+    previousBlockHash:
+      "005c850455f8bf8801a8901d9908cb6acd33bc49c9d718fcf30867c3cbc91ee3"
+  }
+];
+
+console.log("Value: ", bitcoin.chainIsValid(chain));
